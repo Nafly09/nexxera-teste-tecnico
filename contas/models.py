b440bc59d6060322b4443e3702ce3dfb9e2beea7
nafly09 <juniorinterlyte95@hotmail.com>
2022-06-06 17:52:04 -0300

    feat: finished 90% of the project, but waiting for fixxes and refactoring

--- contas/models.py
+++ contas/models.py
@@ -8,7 +8,25 @@ class Accounts(models.Model):
     balance = models.FloatField(default=0.0)
 
 
-class Extracts(models.Model):
+class Extract(models.Model):
     extract_id = models.UUIDField(primary_key=True, editable=False, default=uuid4)
-    extract_date = models.DateTimeField(auto_now=True)
-    accounts = models.ManyToManyField(Accounts, related_name="extracts")
+    extract_date = models.DateTimeField(editable=False, auto_now=True)
+    former_account_balance = models.FloatField()
+    current_account_balance = models.FloatField()
+    extract_account = models.ForeignKey(Accounts, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        self.former_account_balance = self.extract_account.balance
+        super(Extract, self).save(*args, **kwargs)
+
+
+class Transactions(models.Model):
+    transaction_id = models.UUIDField(primary_key=True, editable=False, default=uuid4)
+    transaction_type = models.CharField(max_length=255)
+    transaction_value = models.FloatField()
+    transaction_date = models.DateTimeField(auto_now=True)
+    transaction_description = models.CharField(max_length=255)
+    transaction_account = models.ForeignKey(Accounts, on_delete=models.CASCADE)
+    transaction_extract = models.ForeignKey(
+        Extract, on_delete=models.CASCADE, related_name="transactions"
+    )
--- contas/views.py
+++ contas/views.py
@@ -1,15 +1,22 @@
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.decorators import APIView
+from rest_framework.decorators import APIView, api_view
 from rest_framework.status import (
     HTTP_201_CREATED,
     HTTP_204_NO_CONTENT,
     HTTP_401_UNAUTHORIZED,
     HTTP_403_FORBIDDEN,
     HTTP_422_UNPROCESSABLE_ENTITY,
+    HTTP_404_NOT_FOUND,
+    HTTP_200_OK,
 )
-from contas.models import Accounts
-from contas.serializers import AccountsSerializer, ExtractSerializer
+
+from contas.models import Extract, Transactions, Accounts
+from contas.serializers import (
+    AccountsSerializer,
+    ExtractSerializer,
+    UpdateAccountBalanceSerializer,
+)
 
 
 class AccountsView(APIView):
@@ -32,7 +39,64 @@ class AccountsView(APIView):
         return Response(serializer.data, HTTP_201_CREATED)
 
     def get(self, request: Request):
-        serializer = AccountsSerializer(request.user)
         found_accounts = Accounts.objects.all()
         serializer = [AccountsSerializer(account).data for account in found_accounts]
         return Response(serializer)
+
+    def patch(self, request: Request, action: None, account_id):
+        serializer = UpdateAccountBalanceSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        found_account = Accounts.objects.get(account_id=account_id)
+
+        if not found_account:
+            return Response(
+                {"message": "Account for the requested account_id does not exist"},
+                HTTP_404_NOT_FOUND,
+            )
+        if action == "credit":
+            found_account.balance += serializer.validated_data["balance"]
+
+            current_extract, _ = Extract.objects.update_or_create(
+                extract_account_id=account_id,
+                defaults={"current_account_balance": found_account.balance},
+            )
+
+            transaction = {
+                "transaction_type": "credit",
+                "transaction_value": serializer.validated_data["balance"],
+            }
+            Transactions.objects.create(
+                **transaction,
+                transaction_account=found_account,
+                transaction_extract=current_extract
+            )
+
+        if action == "debit":
+            found_account.balance -= serializer.validated_data["balance"]
+            # new_balance = found_account.balance - serializer.validated_data["balance"]
+
+            current_extract, _ = Extract.objects.update_or_create(
+                extract_account_id=account_id,
+                defaults={"current_account_balance": found_account.balance},
+            )
+
+            transaction = {
+                "transaction_type": "debit",
+                "transaction_value": serializer.validated_data["balance"],
+            }
+            Transactions.objects.create(
+                **transaction,
+                transaction_account=found_account,
+                transaction_extract=current_extract
+            )
+        found_account.save()
+        serializer = AccountsSerializer(found_account)
+
+        return Response(serializer.data, HTTP_200_OK)
+
+
+class ExtractView(APIView):
+    def get(self, _, account_id: None):
+        extract = Extract.objects.get(extract_account_id=account_id)
+        serializer = ExtractSerializer(extract)
+        return Response(serializer.data)
