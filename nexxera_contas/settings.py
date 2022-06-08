54f66f527c4301a519cd8efd54da17dc015e34ce
nafly09 <juniorinterlyte95@hotmail.com>
2022-06-08 16:10:43 -0300

    fix: fixed procfile name, and localhost in allowed hosts

--- nexxera_contas/settings.py
+++ nexxera_contas/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "nexxera-accounts.herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "nexxera-accounts.herokuapp.com"]
 
 
 # Application definition
