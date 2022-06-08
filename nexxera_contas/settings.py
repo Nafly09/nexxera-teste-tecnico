986ce9cf3a4e9389717d5471c47e0813703aa96b
nafly09 <juniorinterlyte95@hotmail.com>
2022-06-08 15:49:00 -0300

    fix: fixed database url

--- nexxera_contas/settings.py
+++ nexxera_contas/settings.py
@@ -88,6 +88,16 @@ DATABASES = {
     }
 }
 
+DATABASE_URL = os.environ.get("DATABASE_URL")
+# Essa variável de ambiente é setada automaticamente pelo Heroku
+
+if DATABASE_URL:
+    db_from_env = dj_database_url.config(
+        default=DATABASE_URL, conn_max_age=500, ssl_require=True
+    )
+    DATABASES["default"].update(db_from_env)
+    DEBUG = False
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
